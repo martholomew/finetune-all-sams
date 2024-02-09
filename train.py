90ee22433d5b94bcf671dbe9c9ad54bfd5aa3c5a
martholomew <martholomew@users.noreply.github.com>
2024-02-09 14:04:19 +0000

    First commit!

---
+++ train.py
@@ -0,0 +1,89 @@
+from statistics import mean
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from utils.dataloader import DatasetSegmentation, collate_fn
+from utils.lora import LoRA_sam
+from utils.processor import Samprocessor
+import argparse
+import monai
+import torch
+import yaml
+
+"""
+This file is used to train a LoRA_sam model. I use that monai DiceLoss for the training. The batch size and number of epochs are taken from the configuration file.
+The model is saved at the end as a safetensor.
+"""
+
+parser = argparse.ArgumentParser(description="SAM-fine-tune Training")
+parser.add_argument("-d", "--device", choices=["cuda", "cpu"], default="cuda", help="What device to run the training on.")
+parser.add_argument("-s", "--sam", choices=["sam", "samfast", "mobilesam", "mobilesamv2"], default="sam", help="What version of SAM to use.")
+
+args = parser.parse_args()
+
+if args.sam == "sam":
+  from segment_anything import build_sam_vit_b
+elif args.sam == "samfast":
+   from segment_anything_fast import build_sam_vit_b
+
+# Load the config file
+with open("./config.yaml", "r") as ymlfile:
+   config_file = yaml.load(ymlfile, Loader=yaml.Loader)
+
+# Take dataset path
+train_dataset_path = config_file["DATASET"]["PATH"]
+# Load SAM model
+sam = build_sam_vit_b(checkpoint=config_file["SAM"]["SAM_VIT_B"])
+#Create SAM LoRA
+sam_lora = LoRA_sam(sam, config_file["LORA"]["RANK"])  
+model = sam_lora.sam
+# Process the dataset
+processor = Samprocessor(model, args.sam)
+train_ds = DatasetSegmentation(config_file, processor, mode="train")
+# Create a dataloader
+train_dataloader = DataLoader(train_ds, batch_size=config_file["TRAIN"]["BATCH_SIZE"], shuffle=True, collate_fn=collate_fn)
+# Initialize optimize and Loss
+optimizer = Adam(model.image_encoder.parameters(), lr=config_file["TRAIN"]["LEARNING_RATE"], weight_decay=0)
+seg_loss = monai.losses.DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
+num_epochs = config_file["TRAIN"]["NUM_EPOCHS"]
+
+if args.device == "cuda":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+else:
+    device = "cpu"
+
+# Set model to train and into the device
+model.train()
+model.to(device)
+
+total_loss = []
+
+for epoch in range(num_epochs):
+    epoch_losses = []
+
+    for i, batch in enumerate(tqdm(train_dataloader)):
+      
+      outputs = model(batched_input=batch,
+                      multimask_output=False)
+
+      stk_out = torch.stack([out["low_res_logits"] for out in outputs], dim=0)
+      stk_gt = torch.stack([b["ground_truth_mask"] for b in batch], dim=0)
+      stk_out = stk_out.squeeze(1)
+      stk_gt = stk_gt.unsqueeze(1) # We need to get the [B, C, H, W] starting from [H, W]
+      loss = seg_loss(stk_out, stk_gt.float().to(device))
+      
+      optimizer.zero_grad()
+      
+      # if args.sam == "samfast":
+      #   loss.requires_grad = True
+      loss.backward()
+      # optimize
+      optimizer.step()
+      epoch_losses.append(loss.item())
+
+    print(f'EPOCH: {epoch}')
+    print(f'Mean loss training: {mean(epoch_losses)}')
+
+# Save the parameters of the model in safetensors format
+rank = config_file["SAM"]["RANK"]
+sam_lora.save_lora_parameters(f"lora_rank{rank}.safetensors")
---
+++ utils/dataloader.py
@@ -0,0 +1,96 @@
+import torch
+from os import listdir
+from os.path import isfile, join
+from PIL import Image
+import numpy as np
+
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+from utils.processor import Samprocessor
+
+
+class DatasetSegmentation(Dataset):
+    """
+    Dataset to process the images and masks
+
+    Arguments:
+        folder_path (str): The path of the folder containing the images
+        processor (obj): Samprocessor class that helps pre processing the image, and prompt 
+    
+    Return:
+        (dict): Dictionnary with 4 keys (image, original_size, boxes, ground_truth_mask)
+            image: image pre processed to 1024x1024 size
+            original_size: Original size of the image before pre processing
+            boxes: bouding box after adapting the coordinates of the pre processed image
+            ground_truth_mask: Ground truth mask
+    """
+
+    def __init__(self, config_file: dict, processor: Samprocessor, mode: str):
+        super().__init__()
+        if mode == "train":
+            path = config_file["DATASET"]["PATH"]
+            image_path = path + "/images/"
+            mask_path = path + "/masks/"
+            self.img_files = [image_path + f for f in listdir(image_path) if isfile(join(image_path, f))]
+            self.mask_files = [mask_path + f for f in listdir(mask_path) if isfile(join(mask_path, f))]
+
+        self.processor = processor
+
+    def __len__(self):
+        return len(self.img_files)
+    
+    def __getitem__(self, index: int) -> list:
+            img_path = self.img_files[index]
+            mask_path = self.mask_files[index]
+            # get image and mask in PIL format
+            image =  Image.open(img_path)
+            mask = Image.open(mask_path)
+            mask = mask.convert('1')
+            ground_truth_mask =  np.array(mask)
+            original_size = tuple(image.size)[::-1]
+    
+            # get bounding box prompt
+            box = get_bounding_box(ground_truth_mask)
+            inputs = self.processor(image, original_size, box)
+            inputs["ground_truth_mask"] = torch.from_numpy(ground_truth_mask)
+
+            return inputs
+    
+def collate_fn(batch: torch.utils.data) -> list:
+    """
+    Used to get a list of dict as output when using a dataloader
+
+    Arguments:
+        batch: The batched dataset
+    
+    Return:
+        (list): list of batched dataset so a list(dict)
+    """
+    return list(batch)
+
+def get_bounding_box(ground_truth_map: np.array) -> list:
+    """
+    Get the bounding box of the image with the ground truth mask
+
+    Arguments:
+        ground_truth_map: Take ground truth mask in array format
+
+    Return:
+        bbox: Bounding box of the mask [X, Y, X, Y]
+
+    """
+    # get bounding box from mask
+    idx = np.where(ground_truth_map > 0)
+    x_indices = idx[1]
+    y_indices = idx[0]
+    x_min, x_max = np.min(x_indices), np.max(x_indices)
+    y_min, y_max = np.min(y_indices), np.max(y_indices)
+    # add perturbation to bounding box coordinates
+    H, W = ground_truth_map.shape
+    x_min = max(0, x_min - np.random.randint(0, 20))
+    x_max = min(W, x_max + np.random.randint(0, 20))
+    y_min = max(0, y_min - np.random.randint(0, 20))
+    y_max = min(H, y_max + np.random.randint(0, 20))
+    bbox = [x_min, y_min, x_max, y_max]
+
+    return bbox
